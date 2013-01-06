13036d1143c5b6142edc94ac5e7bb9c942347279
onaluf <selim@arsever.ch>
2013-01-06 20:21:41 +0100

    - refactor the code to be divided between data, tool and game - the race is now define by a "seed" that can be provided throught the URL hash - changed the way the background moves around

---
+++ game.js
@@ -0,0 +1,354 @@
+// The game itself
+var game = (function(){
+    // -----------------------------
+    // ---  closure scoped vars  ---
+    // -----------------------------
+    var canvas;
+    var context;
+    var keys = [];
+    var startTime;
+    var previousTimestamp;
+    var lastDelta = 0;
+    var currentTimeString = "";
+    
+    
+    var seed = tools.parseHash();
+	
+    var road = [];
+   
+    var player = {
+        position: 10,
+        speed: 0,
+        acceleration: 0.05,
+        deceleration: 0.3,
+        breaking: 0.6,
+        turning: 5.0,
+        posx: 0,
+        maxSpeed: 15
+    };
+    
+    var splashInterval;
+    var gameInterval;
+   	
+    // -----------------------------
+    // -- closure scoped function --
+    // -----------------------------
+    
+    //initialize the game
+    var init = function(){
+        // configure canvas
+        canvas = $("#c")[0];
+        context = canvas.getContext('2d');
+        
+        canvas.height = data.render.height;
+        canvas.width = data.render.width;
+        
+        // nearest neighbor interpollation
+        context.imageSmoothingEnabled = false;
+        context.webkitImageSmoothingEnabled = false;
+        context.mozImageSmoothingEnabled = false;
+        
+        // pseudo full screen mode
+        tools.resize();
+        $(window).resize(tools.resize);    
+        
+        //register key handeling:
+        $(document).keydown(function(e){
+            keys[e.keyCode] = true;
+        });
+        $(document).keyup(function(e){
+            keys[e.keyCode] = false;
+        });
+        
+        // road generation
+        generateRoad(seed);
+    };
+    
+    //renders Splash Frame
+    var renderSplashFrame = function(){
+        context.fillStyle = "rgb(0,0,0)";
+        context.fillRect(0, 0, data.render.width, data.render.height);
+        
+        context.drawImage(spritesheet,  357, 9, 115, 20, 100, 20, 115, 40);
+        
+        tools.draw.string(context, spritesheet, "Instructions:",{x: 100, y: 90});
+        tools.draw.string(context, spritesheet, "space to start, arrows to drive",{x: 30, y: 100});
+        tools.draw.string(context, spritesheet, "Credits:",{x: 120, y: 120});
+        tools.draw.string(context, spritesheet, "code, art: Selim Arsever",{x: 55, y: 130});
+        tools.draw.string(context, spritesheet, "font: spicypixel.net",{x: 70, y: 140});
+        
+        if(keys[32]){
+            clearInterval(splashInterval);
+      		window.requestAnimationFrame(renderGameFrame);
+      		previousTimestamp = Date.now();
+            //gameInterval = setInterval(renderGameFrame, 30);
+            startTime= new Date();
+        }
+    }
+    
+    //renders one frame
+    var renderGameFrame = function(timestamp){
+        window.requestAnimationFrame(renderGameFrame);
+        var delta = timestamp - previousTimestamp;
+        previousTimestamp = timestamp;
+        
+        // --------------------------
+        // -- Update the car state --
+        // --------------------------
+        
+        if (Math.abs(lastDelta) > 130){
+            if (player.speed > 3) {
+                player.speed -= 0.2;
+            }
+        } else {
+            // read acceleration controls
+            if (keys[38]) { // 38 up
+                //player.position += 0.1;
+                player.speed += player.acceleration;
+            } else if (keys[40]) { // 40 down
+                player.speed -= player.breaking;
+            } else {
+                player.speed -= player.deceleration;
+            }
+        }
+        player.speed = Math.max(player.speed, 0); //cannot go in reverse
+        player.speed = Math.min(player.speed, player.maxSpeed); //maximum speed
+        player.position += player.speed * (delta / 30);
+        
+        // car turning
+        if (keys[37]) {
+            // 37 left
+            if(player.speed > 0){
+                player.posx -= player.turning * (delta / 30);
+            }
+            var carSprite = {
+                a: data.sprites.car_4,
+                x: 117,
+                y: 190
+            };
+        } else if (keys[39]) {
+            // 39 right
+            if(player.speed > 0){
+                player.posx += player.turning * (delta / 30);
+            }
+            var carSprite = {
+                a: data.sprites.car_8,
+                x: 125,
+                y: 190
+            };
+        } else {
+            var carSprite = {
+                a: data.sprites.car, 
+                x:125, 
+                y:190
+            };
+        }
+        
+		
+        //drawBackground(-baseOffset);
+
+        var spriteBuffer = [];
+        
+        // --------------------------
+        // --   Render the road    --
+        // --------------------------
+        var absoluteIndex = Math.floor(player.position / data.road.segmentSize);
+        
+        var currentSegmentIndex        = (absoluteIndex - 2) % road.length;
+        var currentSegmentPosition     = (absoluteIndex - 2) * data.road.segmentSize - player.position;
+        var currentSegment             = road[currentSegmentIndex];
+        
+        var lastProjectedHeight        = Number.POSITIVE_INFINITY;
+        var probedDepth                = 0;
+        var counter                    = absoluteIndex % (2 * data.road.segmentPerColor); // for alternating color band
+        
+        var playerPosSegmentHeight     = road[absoluteIndex % road.length].height;
+        var playerPosNextSegmentHeight = road[(absoluteIndex + 1) % road.length].height;
+        var playerPosRelative          = (player.position % data.road.segmentSize) / data.road.segmentSize;
+        var playerHeight               = data.render.camera_height + playerPosSegmentHeight + (playerPosNextSegmentHeight - playerPosSegmentHeight) * playerPosRelative;
+        
+        var baseOffset                 =  currentSegment.curve + (road[(currentSegmentIndex + 1) % road.length].curve - currentSegment.curve) * playerPosRelative;
+        
+        tools.draw.background(context, data.levels.desert, -baseOffset, 10*(playerPosNextSegmentHeight - playerPosSegmentHeight));
+        
+        lastDelta = player.posx - baseOffset*2;
+        
+        var iter = data.render.depthOfField;
+        while (iter--) {
+            // Next Segment:
+            var nextSegmentIndex       = (currentSegmentIndex + 1) % road.length;
+            var nextSegment            = road[nextSegmentIndex];
+            
+            var startProjectedHeight = Math.floor((playerHeight - currentSegment.height) * data.render.camera_distance / (data.render.camera_distance + currentSegmentPosition));
+            var startScaling         = 30 / (data.render.camera_distance + currentSegmentPosition);
+        
+            var endProjectedHeight   = Math.floor((playerHeight - nextSegment.height) * data.render.camera_distance / (data.render.camera_distance + currentSegmentPosition + data.road.segmentSize));
+            var endScaling           = 30 / (data.render.camera_distance + currentSegmentPosition + data.road.segmentSize);
+
+            var currentHeight        = Math.min(lastProjectedHeight, startProjectedHeight);
+            var currentScaling       = startScaling;
+            
+            if(currentHeight > endProjectedHeight){
+               tools.draw.segment(
+               		context,
+                    data.render.height / 2 + currentHeight, 
+                    currentScaling, currentSegment.curve - baseOffset - lastDelta * currentScaling, 
+                    data.render.height / 2 + endProjectedHeight, 
+                    endScaling, 
+                    nextSegment.curve - baseOffset - lastDelta * endScaling, 
+                    counter < data.road.segmentPerColor, currentSegmentIndex == 2 || currentSegmentIndex == (road.length/*TODO*/-data.render.depthOfField));
+            }
+            if(currentSegment.sprite){
+                spriteBuffer.push({
+                    y: data.render.height / 2 + startProjectedHeight, 
+                    x: data.render.width / 2 - currentSegment.sprite.pos * data.render.width * currentScaling + /* */currentSegment.curve - baseOffset - (player.posx - baseOffset*2) * currentScaling,
+                    ymax: data.render.height / 2 + lastProjectedHeight, 
+                    s: 2.5*currentScaling, 
+                    i: currentSegment.sprite.type});
+            }
+            
+            
+            lastProjectedHeight    = currentHeight;
+            
+            probedDepth            = currentSegmentPosition;
+
+            currentSegmentIndex    = nextSegmentIndex;
+            currentSegment         = nextSegment;
+            
+            currentSegmentPosition += data.road.segmentSize;
+            
+            counter = (counter + 1) % (2 * data.road.segmentPerColor);
+            
+            if(absoluteIndex >= road.length-data.render.depthOfField-1){
+	            tools.draw.string(context, spritesheet, "You did it!", {x: 100, y: 20});
+	            tools.draw.string(context, spritesheet, "Press t to tweet your time.", {x: 30, y: 30});
+	            $(window).keydown(function(e){ if(e.keyCode == 84) {location.href="http://twitter.com/home?status="+escape("I've just raced through #racer10k in "+currentTimeString+"!")}});
+	        }
+        }
+        
+        while(sprite = spriteBuffer.pop()) {
+            tools.draw.sprite(context, sprite);
+        }
+        
+        // --------------------------
+        // --     Draw the car     --
+        // --------------------------
+        tools.draw.image(context, carSprite.a, carSprite.x, carSprite.y, 1);
+
+        // --------------------------
+        // --     Draw the hud     --
+        // --------------------------        
+        tools.draw.string(context, spritesheet, ""+Math.round(absoluteIndex/(road.length-data.render.depthOfField)*100)+"%",{x: 287, y: 1});
+        var now = new Date();
+        var diff = now.getTime() - startTime.getTime();
+        
+        var min = Math.floor(diff / 60000);
+        
+        var sec = Math.floor((diff - min * 60000) / 1000); 
+        if(sec < 10) sec = "0" + sec;
+        
+        var mili = Math.floor(diff - min * 60000 - sec * 1000);
+        if(mili < 100) mili = "0" + mili;
+        if(mili < 10) mili = "0" + mili;
+        
+        currentTimeString = ""+min+":"+sec+":"+mili;
+        
+        tools.draw.string(context, spritesheet, currentTimeString, {x: 1, y: 1});
+        var speed = Math.round(player.speed / player.maxSpeed * 200);
+        tools.draw.string(context, spritesheet, ""+speed+"mph", {x: 1, y: 10});
+    };
+    
+    // -------------------------------------
+    // ---  Generates the road randomly  ---
+    // -------------------------------------
+    var generateRoad = function(seed){
+    	var level = tools.parseSeed(seed);
+    	var r = new tools.r(level.random);
+    	
+        var currentStateH = 0; //0=flat 1=up 2= down
+        var transitionH   = [[0,1,2],[0,2,2],[0,1,1]];
+        
+        var currentStateC = 0; //0=straight 1=left 2= right
+        var transitionC   = [[0,1,2],[0,2,2],[0,1,1]];
+
+        var currentHeight = 0;
+        var currentCurve  = 0;
+
+        var zones         = level.length;
+        while(zones--){
+            // Generate current Zone
+            var finalHeight;
+            switch(currentStateH){
+                case 0:
+                    finalHeight = 0; 
+                    break;
+                case 1:
+                    finalHeight = data.road.maxHeight * r.nextFloat();
+                    break;
+                case 2:
+                    finalHeight = - data.road.maxHeight * r.nextFloat();
+                    break;
+            }
+            var finalCurve;
+            switch(currentStateC){
+                case 0:
+                    finalCurve = 0; break;
+                case 1:
+                    finalCurve = - data.road.maxCurve * r.nextFloat();
+                    break;
+                case 2:
+                    finalCurve = data.road.maxCurve * r.nextFloat();
+                    break;
+            }
+
+            for(var i=0; i < data.road.zoneSize; i++){
+                // add a tree
+                if(i % data.road.zoneSize / 4 == 0){
+                    var sprite = {type: data.sprites.rock, pos: -0.55};
+                } else {
+                    if(r.nextFloat() < 0.05) {
+                        var spriteType = data.levels[level.type].sprites[0];//([tree,rock])[Math.floor(r()*1.9)];
+                        var sprite = {type: spriteType, pos: 0.6 + r.nextRange(0,4)};
+                        if(r.nextFloat() < 0.5){
+                            sprite.pos = -sprite.pos;
+                        }
+                    } else {
+                        var sprite = false;
+                    }
+                }
+                road.push({
+                    height: currentHeight+finalHeight / 2 * (1 + Math.sin(i/data.road.zoneSize * Math.PI-Math.PI/2)),
+                    curve: currentCurve+finalCurve / 2 * (1 + Math.sin(i/data.road.zoneSize * Math.PI-Math.PI/2)),
+                    sprite: sprite
+                })
+            }
+            currentHeight += finalHeight;
+            currentCurve += finalCurve;
+            // Find next zone
+            if(r.nextFloat() < level.mountainy){
+                currentStateH = transitionH[currentStateH][1+Math.round(r.nextFloat())];
+            } else {
+                currentStateH = transitionH[currentStateH][0];
+            }
+            if(r.nextFloat() < level.curvy){
+                currentStateC = transitionC[currentStateC][1+Math.round(r.nextFloat())];
+            } else {
+                currentStateC = transitionC[currentStateC][0];
+            }
+        }
+    };
+    
+    return {
+        start: function(){
+            init();
+            spritesheet = new Image();
+            spritesheet.onload = function(){
+                splashInterval = setInterval(renderSplashFrame, 30);
+            };
+            spritesheet.src = "spritesheet.high.png";
+        }
+    }
+}());
+$(function(){
+    game.start();
+});
---
+++ tools.js
@@ -0,0 +1,221 @@
+/**
+ * This is the file that holds the tools needed for the game.
+ * 
+ * Copyright (c) 2013 Selim Arsever (http://selim.arsever.ch/) 
+ * Under the MIT license.
+ */
+
+// namespace
+var tools = function(){};
+
+// RequestAnimationFrame polyfill by Paul Irish and Erik Möller 
+// (http://paulirish.com/2011/requestanimationframe-for-smart-animating/)
+(function() {
+    var lastTime = 0;
+    var vendors = ['ms', 'moz', 'webkit', 'o'];
+    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+        window.cancelAnimationFrame = 
+          window[vendors[x]+'CancelAnimationFrame'] || window[vendors[x]+'CancelRequestAnimationFrame'];
+    }
+ 
+    if (!window.requestAnimationFrame)
+        window.requestAnimationFrame = function(callback, element) {
+            var currTime = new Date().getTime();
+            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+            var id = window.setTimeout(function() { callback(currTime + timeToCall); }, 
+              timeToCall);
+            lastTime = currTime + timeToCall;
+            return id;
+        };
+ 
+    if (!window.cancelAnimationFrame)
+        window.cancelAnimationFrame = function(id) {
+            clearTimeout(id);
+        };
+}());
+
+// LCG Pseudo Random Number Generator by orip
+// (http://stackoverflow.com/questions/424292/how-to-create-my-own-javascript-random-number-generator-that-i-can-also-set-the#424445)	
+tools.r = function(seed) {
+		// LCG using GCC's constants
+		this.m = 0x100000000; // 2**32;
+		this.a = 1103515245;
+		this.c = 12345;
+	
+		this.state = seed ? seed : Math.floor(Math.random() * (this.m-1));
+	  
+		this.nextInt = function() {
+			this.state = (this.a * this.state + this.c) % this.m;
+			return this.state;
+		}
+		
+		this.nextFloat = function() {
+			// returns in range [0,1]
+			return this.nextInt() / (this.m - 1);
+		}
+		
+		this.nextRange = function(start, end) {
+			// returns in range [start, end): including start, excluding end
+			// can't modulu nextInt because of weak randomness in lower bits
+			var rangeSize = end - start;
+			var randomUnder1 = this.nextInt() / this.m;
+			return start + Math.floor(randomUnder1 * rangeSize);
+		}
+		
+		this.choice = function(array) {
+			return array[this.nextRange(0, array.length)];
+		}
+	}
+	
+// parse the hash part of the url.
+tools.parseHash  = function(){
+	var defaultValue = {
+		seed: "000ZZZZ0"
+	}
+	// TODO this is just a test implementation, more to come
+	if (window.location.hash !== undefined && window.location.hash.length === 9) {
+		defaultValue.seed = window.location.hash.slice(1,9);
+	} 
+	return defaultValue.seed;
+}
+
+// From now on seeds will replace roadParam. A seed is a string made of a
+// a series of characters ranging from '0' to 'Z' (all number and captial  
+// letters), each one with a given meaning:
+// 0-1) a random seed
+// 2) the type of the race:
+//     - '1': desert
+//     - '2': forest
+//     - '3': swamp
+//     - '4': dawn
+//     - '5': fog
+//     - '6': rain
+//     - '7': snow
+//     - '8': ...
+// 3) the length
+// 4) the curvyness
+// 5) the steepness
+// 6) the density of the scenery
+// 7) the difficulty (time or adversary)
+tools.parseSeed = function(seed){
+	var toInt = function(char){
+		// '0' = 48 -> '9' = 57
+		// 'A' = 65 -> 'Z' = 90
+		var int = char.charCodeAt(0) - 48;
+		if(int > 9) {
+			int -= 7;
+		}
+		return int;
+	}
+	var racesType = ["desert", "forest", "swamp",  "dawn", "fog", "rain", "snow"];
+	
+	return {
+		random     : seed[0] + seed[1] * 36,
+		type       : racesType[toInt(seed[2])],
+		length     : toInt(seed[3]) + 1,
+		curvy      : toInt(seed[4]) / 36.0,
+		mountainy  : toInt(seed[5]) / 36.0,
+		density    : toInt(seed[6]) / 36.0,
+		difficulty : toInt(seed[7])
+	}
+}
+
+tools.generateSeed = function(options){
+	return "TODO";
+}
+
+// resize the canvas to make it take the full browser's window
+tools.resize = function(){
+    if ($(window).width() / $(window).height() > data.render.width / data.render.height) {
+        var scale = $(window).height() / data.render.height;
+    }
+    else {
+        var scale = $(window).width() / data.render.width;
+    }
+    
+    var transform = "scale(" + scale + ")";
+    $("#c").css("MozTransform", transform).css("transform", transform).css("WebkitTransform", transform).css({
+        top: (scale - 1) * data.render.height / 2,
+        left: (scale - 1) * data.render.width / 2 + ($(window).width() - data.render.width * scale) / 2
+    });
+};
+
+// Draw function: function that draw something on screen
+tools.draw = {
+	// Drawing primitive
+    image: function(context, image, x, y, scale){
+        context.drawImage(spritesheet,  image.x, image.y, image.w, image.h, x, y, scale*image.w, scale*image.h);
+    },
+    sprite: function(context, sprite){
+        //if(sprite.y <= sprite.ymax){
+            var destY = sprite.y - sprite.i.h * sprite.s;
+            if(sprite.ymax < sprite.y) {
+                var h = Math.min(sprite.i.h * (sprite.ymax - destY) / (sprite.i.h * sprite.s), sprite.i.h);
+            } else {
+                var h = sprite.i.h; 
+            }
+            //sprite.y - sprite.i.h * sprite.s
+            if(h > 0) context.drawImage(spritesheet,  sprite.i.x, sprite.i.y, sprite.i.w, h, sprite.x, destY, sprite.s * sprite.i.w, sprite.s * h);
+        //}
+    },
+    segment: function (context, position1, scale1, offset1, position2, scale2, offset2, alternate, finishStart){
+        var grass     = (alternate) ? "#eda" : "#dc9";
+        var border    = (alternate) ? "#e00" : "#fff";
+        var road      = (alternate) ? "#999" : "#777";
+        var lane      = (alternate) ? "#fff" : "#777";
+
+        if(finishStart){
+            road = "#fff";
+            lane = "#fff";
+            border = "#fff";
+        }
+
+        //draw grass:
+        context.fillStyle = grass;
+        context.fillRect(0, position2, data.render.width,(position1-position2));
+        
+        // draw the road
+        tools.draw.trapez(context, position1, scale1, offset1, position2, scale2, offset2, -0.5, 0.5, road);
+        
+        //draw the road border
+        tools.draw.trapez(context, position1, scale1, offset1, position2, scale2, offset2, -0.5, -0.47, border);
+        tools.draw.trapez(context, position1, scale1, offset1, position2, scale2, offset2, 0.47,   0.5, border);
+        
+        // draw the lane line
+        tools.draw.trapez(context, position1, scale1, offset1, position2, scale2, offset2, -0.18, -0.15, lane);
+        tools.draw.trapez(context, position1, scale1, offset1, position2, scale2, offset2,  0.15,  0.18, lane);
+    },
+    trapez: function(context, pos1, scale1, offset1, pos2, scale2, offset2, delta1, delta2, color){
+        var demiWidth = data.render.width / 2;
+        
+        context.fillStyle = color;
+        context.beginPath();
+        context.moveTo(demiWidth + delta1 * data.render.width * scale1 + offset1, pos1);
+        context.lineTo(demiWidth + delta1 * data.render.width * scale2 + offset2, pos2); 
+        context.lineTo(demiWidth + delta2 * data.render.width * scale2 + offset2, pos2); 
+        context.lineTo(demiWidth + delta2 * data.render.width * scale1 + offset1, pos1);
+        context.fill();
+    },
+    background: function(context, level, position, height) {
+    	// Clean screen
+        context.fillStyle = level.colors.sky;
+        context.fillRect(0, 0, data.render.width, height);
+        
+        context.fillStyle = level.colors.ground1;
+        context.fillRect(0, level.background.h + height, data.render.width, data.render.height -level.background.h - height);
+        
+        var first = position / 2 % (level.background.w);
+        tools.draw.image(context, level.background, first-level.background.w +1, height, 1);
+        tools.draw.image(context, level.background, first+level.background.w -1, height, 1);
+        tools.draw.image(context, level.background, first, height, 1);
+    },
+    string: function(context, spritesheet, string, pos) {
+        string = string.toUpperCase();
+        var cur = pos.x;
+        for(var i=0; i < string.length; i++) {
+            context.drawImage(spritesheet, (string.charCodeAt(i) - 32) * 8, 0, 8, 8, cur, pos.y, 8, 8);
+            cur += 8;
+        }
+    }
+} 
